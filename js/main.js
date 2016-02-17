9409f1e460526b46192c5f063c71fe81c192b88c
Wfuger <wfuger@gmail.com>
2016-02-16 21:30:07 -0700

    functional

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
 $(function() {
+  var screen = document.getElementById('screen');
   var total = 0;
-  var calculator = [];
-  $('span').on('click', function(e){
-    calculator.push($(this).text());
-  })
-  if (calculator.length === 3) {
+  var firstNum = '';
+  var opp = '';
+  var disp = 0;
 
-  }
+  $('span').on('click', function(e){
+    if($(this).attr('id') == 'cancel'){
+      firstNum = '';
+      opp = '';
+      total = 0;
+      return firstNum, opp, total;
+    }
+    if($(this).attr('id') == 'calc'){
+      if( opp == 'x' ){
+        disp = total * parseInt(firstNum);
+        console.log(disp + 'fuckyeah');
+      }
+      if( opp == '-' ){
+        disp = total - parseInt(firstNum);
+        console.log(disp + 'fuckyeah');
+      }
+      if( opp == '+' ){
+        disp = total + parseInt(firstNum);
+        console.log(disp + 'fuckyeah');
+      }
+      if( opp == '\xF7' ){
+        disp = total / parseInt(firstNum);
+        console.log(disp + 'fuckyeah');
+      }
+    }
+    if($(this).attr('class') == 'operator') {
+      opp += ($(this).text());
+      total = parseInt(firstNum);
+      firstNum = '';
+      console.log(opp + 'h');
+      
+    } else {
+      firstNum += $(this).text();
+      console.log(firstNum + 'f');
+    }
+  })
 })
