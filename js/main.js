dd9b315511308550e2775f1471fa61974b9b9aba
Wfuger <wfuger@gmail.com>
2016-02-16 20:13:56 -0700

    Collecting values of buttons in array

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+  var total = 0;
+  var calculator = [];
+  $('span').on('click', function(e){
+    calculator.push($(this).text());
+  })
+  if (calculator.length === 3) {
+
+  }
+})
