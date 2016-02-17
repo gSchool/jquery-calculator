f73531d1271a240c545ef962d99def6b0c3cd3f6
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-17 10:05:13 -0700

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 
 var equation = '';
+var showEquation = '';
 
 $('.buttons').children().on('click', function(e) {
 
   if (e.target.textContent === '=') {
     equation = eval(equation);
+    showEquation = equation
   } else if (e.target.textContent === 'C') {
     equation = '';
+    showEquation = '';
   } else if (e.target.textContent === 'x') {
     equation += '*';
-  } else if (e.target.textContent === '&divide;') {
+    showEquation += 'x'
+  } else if (e.target.textContent === '\xF7') {
     equation += '/';
+    showEquation += '\xF7'
   } else {
     equation += (e.target.textContent)
+    showEquation += (e.target.textContent)
   }
-
-  $('#screen').html(equation)
+  $('#screen').html(showEquation)
 })
