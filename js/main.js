40eca8d441b5f1d61228b3e9e030271ffa260dab
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-02-17 21:41:48 -0700

    OMG DONE

--- js/main.js
+++ js/main.js
@@ -2,30 +2,44 @@ $(document).ready(function(){
 
   var info;
   var answer = [];
+  var fin = [];
 
   $('span').on('click', function(event){
     info = event.target.innerText;
+    var divide1 = document.getElementsByClassName('operator')[1].innerText;
 
-    if(info !== '='){
+    if(fin.length != 0){
+      $('#screen').html("");
+      fin.splice(0, 1);
+    };
 
+    if((info !== '=')&&(info !== 'C')&&(info !== 'x')&&(info !== divide1)){
       $('#screen').append(info);
       answer.push(info);
       console.log(answer);
-
-    }
-
+    }else if(info == 'x'){
+      $('#screen').append('x');
+      answer.push('*');
+    }else if(info == divide1){
+      $('#screen').append('&divide;');
+      answer.push('/');
+    };
+    console.log(fin);
   });
 
   $('#calc').on('click', function(event){
-    anstring = answer.join(" ");
+    anstring = answer.join("");
     ans = eval(anstring)
-
+    $('#screen').html("");
     $('#screen').append(ans);
+    answer.length = 0;
+    fin.push('1');
+    console.log(fin);
   });
 
-
-
-
-
+  $('#cancel').on('click', function(event){
+    $('#screen').html("");
+    answer.length = 0;
+  });
 
 });
