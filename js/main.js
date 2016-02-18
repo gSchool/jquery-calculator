ced10cca1060bded565aa22930c7ed419bc03a58
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-02-17 19:20:31 -0700

    IT DOES FUCKING MATH

--- js/main.js
+++ js/main.js
@@ -3,23 +3,28 @@ $(function () {
   $('.buttons').on('click', function(event){
 
     var buttonStuff = event.target.innerHTML
+    var answer = document.getElementById("screen").innerHTML;
+    var superAnswer
+    var divideSucksMyAss = document.getElementsByClassName("operator")[1].innerHTML
 
-    if ( buttonStuff < 10 ) {
 
-      $("#screen").append(buttonStuff);
-
-    }
+    if (buttonStuff ==="x") {
+      $('#screen').append('*');
+    } else if (buttonStuff == divideSucksMyAss)
+      {$('#screen').append("/");
+    } else {
+    $("#screen").append(buttonStuff);
+  }
 
     if ( buttonStuff === 'C' ) {
-
       $("#screen").empty();
-
     }
 
-    if ( event.target.className === 'operator' && buttonStuff !== 'C') {
-      $("#screen").append(buttonStuff);
+    if (buttonStuff === "=") {
+      superAnswer = eval(answer);
+      $('#screen').text(superAnswer)
 
-    };
+    }
 
   })
 
