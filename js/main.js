6717e59f2eca542bc922314d08db4eefb3d1298f
Mickey <develop.agility@gmail.com>
2016-02-17 20:19:04 -0700

    a lil bit cleaner

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(function(){
 
-  var $screen = $('#screen');
+  var screen = $('#screen');
 
   $(".buttons").on('mousedown', function(e){
 
-    if($(e.target).is($('#calc'))){
-      $screen.text(eval($screen.text()));
+    if($(e.target).is($('#calc')))
+      screen.text(eval(screen.text()));
 
-    } else if($(e.target).is($('#cancel'))){
-      $screen.empty();
+    else if($(e.target).is($('#cancel')))
+      screen.empty();
 
-    } else
-      $screen.append($(e.target).text());
+    else
+      screen.append($(e.target).text());
   });
 });
