c20a60b05dd3d57820a5809ac1dd4c669fbc588f
Mickey <develop.agility@gmail.com>
2016-02-17 20:03:39 -0700

    ultra simplified

--- js/main.js
+++ js/main.js
@@ -4,34 +4,13 @@ $(function(){
 
   $(".buttons").on('mousedown', function(e){
 
-    function addToScreen(target){
+    if($(e.target).is($('#calc'))){
+      $screen.text(eval($screen.text()));
 
-      if($(e.target).is(target))
-        $screen.append($(e.target).text());
-    }
+    } else if($(e.target).is($('#cancel'))){
+      $screen.empty();
 
-    addToScreen($("span:contains(0)"));
-    addToScreen($("span:contains(1)"));
-    addToScreen($("span:contains(2)"));
-    addToScreen($("span:contains(3)"));
-    addToScreen($("span:contains(4)"));
-    addToScreen($("span:contains(5)"));
-    addToScreen($("span:contains(6)"));
-    addToScreen($("span:contains(7)"));
-    addToScreen($("span:contains(8)"));
-    addToScreen($("span:contains(9)"));
-
-    addToScreen($(".operator:contains('*')"));
-    addToScreen($(".operator:contains('/')"));
-    addToScreen($(".operator:contains('+')"));
-    addToScreen($(".operator:contains('-')"));
-  });
-
-  $('#calc').on('mousedown', function(){
-    $screen.text(eval($screen.text()));
-  });
-
-  $('#cancel').on('mousedown', function(){
-    $screen.empty();
+    } else
+      $screen.append($(e.target).text());
   });
 });
