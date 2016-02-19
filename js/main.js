f1c9a0b46580980f7fdada6b1b09aee1dbf7986a
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-18 22:03:47 -0700

    Calculator working, but doesn't reset after '= ' is pressed.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   var equation = "";
 
-  $('span').on('click', function(event){
-    if(event.target.textContent === "C") {
+  $('span').on('click', function(event) {
+    if (event.target.textContent === "C") {
       equation = "";
-    } else if(event.target.textContent === "x") {
+    } else if (event.target.textContent === "x") {
       equation += "*";
-    } else if(event.target.textContent === "&divide;") {
+    } else if (event.target.textContent === "&divide;") {
       equation += "/";
-    } else if(event.target.id === "calc") {
+    } else if (event.target.id === "calc") {
       equation = eval(equation);
     } else {
       equation += (event.target.textContent);
