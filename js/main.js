05611ade8598e697a56ce82c44ea1601f1984f67
Summer Wollin <summer.wollin@gmail.com>
2016-02-18 17:45:44 -0700

    Include new feature for after evaluation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(function() {
+  var evaluated = false;
   $('span').not('.operator').on('click', function() {
+    if (evaluated === false) {
     $("#screen").append($(this).text());
+  }
   })
   $('.operator').on('click', function() {
+    if (evaluated === true) {
+      evaluated = false;
+    }
     if ($(this).text() === '\u00f7') {
       $("#screen").append('/');
     }
@@ -16,6 +22,7 @@ $(function() {
   $('#calc').on('click', function() {
     var str = $("#screen").text().toString();
     $("#screen").text(eval(str));
+    evaluated = true;
   })
   $('#cancel').on('click', function () {
     $("#screen").text("");
