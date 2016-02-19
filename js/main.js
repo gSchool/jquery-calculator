7b48275ec3b13a939f1773dc2294d7ea6a8fb21e
Summer Wollin <summer.wollin@gmail.com>
2016-02-18 17:09:41 -0700

    Include solution for calculator in main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('span').not('.operator').on('click', function() {
+    $("#screen").append($(this).text());
+  })
+  $('.operator').on('click', function() {
+    if ($(this).text() === '\u00f7') {
+      $("#screen").append('/');
+    }
+    if ($(this).text() === 'x') {
+      $("#screen").append('*');
+    }
+    else if ($(this).text() !== '=') {
+    $("#screen").append($(this).text());
+  }
+  })
+  $('#calc').on('click', function() {
+    var str = $("#screen").text().toString();
+    $("#screen").text(eval(str));
+  })
+  $('#cancel').on('click', function () {
+    $("#screen").text("");
+  })
+});
