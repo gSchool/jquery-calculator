b6cf52e4dcbd3ed2c9e6f7eee457b4b8a7262adf
Anthony Simpson <ansi6622@colorado.edu>
2016-02-19 15:34:40 -0700

    yay calculator

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
-$(function(){
-  var equation = '';
-  var numOkay = true
-  var optTrue = false
+$(function() {
+  var expression = '';
+  var numb = true;
+  var switcha = false;
 
   $('.buttons').children().on('click', function(e) {
-    var clk = e.target.textContent
+    var btnPressed = e.target.textContent
 
-    if ( clk === '=') {
-      if (optTrue) {
-        equation = eval(equation);
-        numOkay = false;
-        optTrue = false;
+    if (btnPressed === '=') {
+      if (switcha) {
+        expression = eval(expression);
+        numb = false;
+        switcha = false;
       }
-    } else if (clk === 'C') {
-      equation = '';
-      numOkay = true;
-      optTrue = false;
+    } else if (btnPressed === 'C') {
+      expression = '';
+      numb = true;
+      switcha = false;
 
-    } else if (clk === 'x') {
-      equation += '*';
-      optTrue = true;
+    } else if (btnPressed === 'x') {
+      expression += '*';
+      switcha = true;
 
-    } else if (clk === '\xF7') {
-      equation += '/';
-      optTrue = true
+    } else if (btnPressed === '\xF7') {
+      expression += '/';
+      switcha = true
 
-    } else if (clk === '+') {
-      equation += '+';
-      optTrue = true;
+    } else if (btnPressed === '+') {
+      expression += '+';
+      switcha = true;
 
-    } else if (clk === '-') {
-      equation += '-';
-      optTrue = true;
+    } else if (btnPressed === '-') {
+      expression += '-';
+      switcha = true;
 
     } else {
-      if (numOkay === true || optTrue === true) {
-        equation += (clk);
+      if (numb === true || switcha === true) {
+        expression += (btnPressed);
 
       }
     }
-    $('#screen').html(equation)
+    $('#screen').html(expression)
   });
 });
