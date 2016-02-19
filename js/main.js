c82b82b0fe1c0049fc0b4c61a35a1295db0fa99c
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-18 17:10:50 -0700

    Calculator working

--- js/main.js
+++ js/main.js
@@ -3,22 +3,17 @@ $(document).ready(function(){
   var equation = "";
 
   $('span').on('click', function(event){
-    if(event.target.textContent === "=") {
-      console.log(eval(equation));
-    } else if(event.target.textContent === "C") {
+    if(event.target.textContent === "C") {
       equation = "";
-      console.log(equation);
     } else if(event.target.textContent === "x") {
       equation += "*";
-      console.log(equation);
     } else if(event.target.textContent === "&divide;") {
       equation += "/";
-      console.log(equation);
+    } else if(event.target.id === "calc") {
+      equation = eval(equation);
     } else {
       equation += (event.target.textContent);
-      console.log(equation);
     }
-    // GET SCREEN WORKING
-    // $('#screen').textContent = equation;
+    $('#screen').text(equation);
   });
 })
