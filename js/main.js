e8f015616b389e313fc47653fee439c4ef651c3d
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-21 14:26:21 -0700

    trying to split string into array

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
   $( "#button-container span" ).on('click', function( event ) {
     console.log(event);
     var info = $(event.target).text();
-    var calcClass = event.target.className;
+    console.log(info);
+    var hiddenInfo = '';
     var calcID = event.target.id;
 
 
@@ -15,12 +16,28 @@ if(calcID === 'cancel'){
 
   var formula = $('#screen').text();
   var newVar = eval(formula);
-  console.log(newVar);
-  console.log(formula);
+  // console.log(newVar);
+  // console.log(formula);
   $('#screen').text(newVar);
+
+  // var regex = /([\+\x\\u00F7\-])/;
+  // var a = formula.split('[\+\x\u00F7\-]');
+  //
+  // console.log(a);
+
+
 
 } else {
   $('#screen').append(info);
+
+  if (info === '\u00F7') {
+           += '/';
+      } else if (buttons[i].innerHTML === 'x') {
+         result.innerHTML  += '*';
+      } else {
+         result.innerHTML  += buttons[i].innerHTML;
+      }
+
 }
 
 
