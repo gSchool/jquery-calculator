8c89f853691ac4825aa377e5945fabccdbc07ceb
lucas barbula <lucas.barbula@gmail.com>
2016-05-03 08:40:57 -0600

    working with regex

--- js/main.js
+++ js/main.js
@@ -8,13 +8,20 @@ $(document).ready(function(){
   var evaluate = "";
   $('#calc').click(function(){
     evaluate = $('#screen').text();
+    evaluate = evaluate.replace("++", "+");
+    evaluate = evaluate.replace("--", "-");
     evaluate = evaluate.replace("=", " ");
-    evaluate = evaluate.replace("x", "*");
+    evaluate = evaluate.replace(/x/g, "*");
     evaluate = evaluate.replace("\u00F7", "/");
     var total = eval(evaluate);
-    //console.log(total);
-    $('#screen').append(total).text();
-  })
+    if(total === Infinity) {
+      total = "Undefined";
+    }
+    else {
+      total = total;
+    }
+    $('#screen').text(total);
+})
   $("#cancel").click(function(){
     $('#screen').text("");
   })
