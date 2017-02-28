79396f95899831b7520b37eaa8dfd91429fb0dc7
lrobinsonco <lrobinsonco@gmail.com>
2017-02-28 16:14:00 -0700

    got working

--- js/index.js
+++ js/index.js
@@ -1,55 +1,31 @@
+var answer = "";
 $(document).ready(function() {
     console.log("ready!");
     function calc(){
-      var screen = $('#screen').text();
-      var operator = " ";
-      var num1 = " ";
-      var num2 = " ";
-      for (var i = 0; i < screen.length; i++) {
-      if (screen[i]){
-      }
+      answer = answer.replace('x','*');
+      answer = answer.replace('\xF7', '/');
+      var result = eval(answer);
+      $('#screen').text(result);
     }
 
 
 
     $('span').click(function() {
+
         // The .attr() method gets the attribute value for only the first element in the matched set.
-        if ($('span').attr('id') === "equals") {
+        if ($(this).attr('id') === "equals") {
             calc();
-        } else if ($('span').attr('id') === "clear") {
+            console.log("calculator");
+        } else if ($(this).attr('id') === "clear") {
+          console.log("clear");
             $('#screen').text(" ");
         } else {
-            $('#screen').append($(span).text());
+            answer += $(this).text();
+            console.log(answer);
+            $('#screen').text(answer);
         }
     });
-}
-// $(document).ready(function(){
-//   var currentString = ''
-//   $('span').not('#equals').click(function(){
-//     currentString += $(this).text();
-//     $('#screen').text(currentString)
-//     console.log(currentString)
-//   })
-//   $('#clear').click(function(){
-//     currentString = ''
-//     $('#screen').text(currentString)
-//   })
-//   $('#equals').click(function(){
-//      var replace = currentString.replace('x', '*')
-//      var replace2 = replace.replace('\xF7', '/')
-//      var answer = eval(replace2)
-//      console.log(answer)
-//     $('#screen').text(answer)
-//     if ($('#screen').text()===('Infinity')){
-//       $('#screen').text('error')
-//     }
-//   })
-// })
-// The .attr() method gets the attribute value for only the first element in the matched set. To get the value for each element individually, use a looping construct such as jQuery's .each() or .map() method.
-//
-// Using jQuery's .attr() method to get the value of an element's attribute has two main benefits:
-//
-// Convenience: It can be called directly on a jQuery object and chained to other jQuery methods
+});
 
 // number variables
 // var add = function(number1, number2) {
@@ -67,15 +43,3 @@ $(document).ready(function() {
 // var divide = function(number1, number2) {
 //   return number1 / number2;
 // };
-
-// $(document).ready(function() {
-//   $("form#add").submit(function(event) {
-//     event.preventDefault();
-//     var number1 = parseInt($("#add1").val());
-//     var number2 = parseInt($("#add2").val());
-//     var result = add(number1, number2);
-//     $("#output").text(result);
-//   });
-// });
-
-// We still need parseInt() because, like the information we gathered with prompt() dialog boxes, content gathered from form inputs is also always in string format, and our calculator's add() function requires numbers
