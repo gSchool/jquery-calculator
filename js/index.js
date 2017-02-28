63a5d48b98ae964df1b7e3f522c7490a96fa01ea
devin hanaway <devinh11@gmail.com>
2017-02-27 19:10:41 -0700

    calcualtor works

---
+++ js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+  // var num1 = ''
+  // var num2 = ''
+  // var op = ''
+
+  // Number(num1) + op +
+
+  var currentString = ""
+  var answer = 8
+  $("span").not('#equals').click(function(){
+    currentString += $(this).text();
+    $("#screen").text(currentString)
+  })
+
+
+  $("#clear").click(function(){
+    currentString = '';
+    $("#screen").text(currentString)
+  })
+
+  $('#equals').click(function(){
+    var replaceX = currentString.replace('x','*')
+    var total = replaceX.replace('\xF7','/')
+    var answer = eval(total)
+    $('#screen').text(answer)
+    if ($('#screen').text()==="Infinity"){
+      $('#screen').text('error')
+    }
+  })
+
+
+
+})
+
+// function updateScreen(a,b,c){
+//   $('#screen').html(a+b+c)
+// }
