3969aeb6d21e84a9f19c61c119007c06c41e7808
lrobinsonco <lrobinsonco@gmail.com>
2017-02-27 21:27:11 -0700

    screen

--- js/index.js
+++ js/index.js
@@ -2,5 +2,23 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-    
+
+
 });
+
+// number variables
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+//
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+//
+// var multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
+//
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
