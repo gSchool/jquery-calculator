1467c0ddcff9b1a0271c4f99639900ed015bb311
devin hanaway <devinh11@gmail.com>
2017-02-28 18:41:49 -0700

    added incorrect format error

--- js/index.js
+++ js/index.js
@@ -1,38 +1,62 @@
 $(document).ready(function(){
 
-  // var num1 = ''
-  // var num2 = ''
-  // var op = ''
-
-  // Number(num1) + op +
-
   var currentString = ""
   var answer = 8
+  var lastClick = false
+
   $("span").not('#equals').click(function(){
-    currentString += $(this).text();
-    $("#screen").text(currentString)
+    if (currentString === 'error'){
+      $('#screen').text(currentString)
+    }
+      else
+        if (lastClick === true && $(this).hasClass('operator')){
+          currentString = 'error'
+          $('#screen').text(currentString)
+        }
+      else {
+        currentString += $(this).text();
+        $("#screen").text(currentString)
+        if ($(this).hasClass('operator')){
+          lastClick = true
+        }
+          else {
+            lastClick = false
+          }
+      }
+
   })
 
 
   $("#clear").click(function(){
-    currentString = '';
+    currentString = ''
     $("#screen").text(currentString)
   })
 
   $('#equals').click(function(){
+    if (currentString === 'error'){
+      $('#screen').text(currentString)
+    }
+    else
+      if (lastClick === true){
+        currentString = 'error'
+        $('#screen').text(currentString)
+    }
     var replaceX = currentString.replace('x','*')
     var total = replaceX.replace('\xF7','/')
     var answer = eval(total)
     currentString = answer
       $('#screen').text(answer)
       if ($('#screen').text()==="Infinity" ){
-        $('#screen').text('error')
+        currentString = 'error'
+        $('#screen').text(currentString)
       // for (var i = 0; i < currentString.length; i++){
       //   if (currentString.charAt(i)===currentString.charAt(i-1)){
       //     $('#screen').text('error')
       //   }
       // }
       }
+
+
 
 
 
