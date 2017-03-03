5459fce79f1d86685c74c393a637b9cb7fc7287a
Michelle Bergquist <caker25@gmail.com>
2017-03-02 22:31:54 -0700

    Add updated calc

--- js/index.js
+++ js/index.js
@@ -1,26 +1,63 @@
-var currentScreen = "";
 
-// saves clicked item to currentScreen string
-// updates the screen to display full currentScreen string
-$('span').click(function(){
+var currentScreen = ""
+var firstNum = 0
+var operator = ""
+var secondNum = 0
+
+$('span').click(function() {
   var entry = $(this).text()
-  if (entry != "=") {
-    currentScreen += entry
+  if (entry === "\xF7" || entry === "+" || entry === "x" || entry === "-") {
+    if (operator === "") {
+      operator = entry
+      currentScreen += entry
+      $('#screen').text(currentScreen)
+    }
+    return
   }
-  $('#screen').text(currentScreen)
+  if (entry !== "=") {
+    if (operator === "") {
+      var entryStr = parseInt(entry)
+      firstNum = (firstNum * 10 + entryStr)
+    } else {
+      var entryStr = parseInt(entry)
+      secondNum = (secondNum * 10 + entryStr)
+    }
+    currentScreen += entry
+    $('#screen').text(currentScreen)
+  }
 })
 
-$('#clear').click(function(){
+
+$('#clear').click(function() {
   currentScreen = ""
+  firstNum = 0
+  operator = ""
+  secondNum = 0
   $('#screen').text(currentScreen)
 });
 
-$('#equals').click(function(){
-  currentScreen = currentScreen.replace('x','*')
-  currentScreen = currentScreen.replace('\xF7','/')
-  var answer = eval(currentScreen)
-    $('#screen').text(answer)
-    if ($('#screen').text() === "Infinity"){
-    $('#screen').text("Error")
+$('#equals').click(function() {
+  var answer
+  switch(operator) {
+    case "+":
+      answer = firstNum + secondNum
+      break;
+   case "-":
+       answer = firstNum - secondNum
+       break;
+   case "x":
+       answer = firstNum * secondNum
+       break;
+   case "\xF7":
+       answer = firstNum / secondNum
+       break;
   }
+    currentScreen = answer
+    $('#screen').text(currentScreen)
+    firstNum = answer
+    operator = ""
+    secondNum = 0
+    if ($('#screen').text() === "Infinity"){
+      $('#screen').text("Error")
+    }
 })
